99ddb4678f4d67cbb2a03048d5e6983c8cc159d9
robsyc <robbe.claeys@gmail.com>
2024-07-24 17:17:28 +0200

    repaired connection to DB

--- api/connection.py
+++ api/connection.py
@@ -16,7 +16,7 @@ import os
 load_dotenv() # Load environment variables from .env file
 NEO4J_URI = os.getenv("NEO4J_URI", default = "neo4j://localhost:7687")
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME", default = "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", default = "password")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", default = "secret")
 if not (NEO4J_URI and NEO4J_USERNAME and NEO4J_PASSWORD):
     raise ValueError(
         "One or more .env variables are not set: NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD"
