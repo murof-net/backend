ca1b74a896f556ddf8cf6edf8eab3e782e5d456a
robsyc <robbe.claeys@gmail.com>
2024-07-30 22:42:52 +0200

    chore: Remove unused code files and models

--- old/models/social/email.py
+++
@@ -1,9 +0,0 @@
-from neomodel import StructuredNode, EmailProperty
-
-class Email(StructuredNode):
-    """
-    Email node in the Neo4j database.
-    Properties:
-        email (str): The email address of the user. Used as the primary key.
-    """
-    email = EmailProperty(required=True, unique_index=True)
