05da70ac947a55fef604e33086c5c10baecf98c0
robsyc <robbe.claeys@gmail.com>
2024-09-23 15:00:17 +0200

    First work on auth with email!

--- api/routes/auth/auth.py
+++ api/routes/auth/auth.py
@@ -1,10 +1,11 @@
 import os
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from .schemas import RegistrationForm, LoginForm, Token
+from fastapi.security import OAuth2PasswordRequestForm
+from .schemas import RegistrationForm, Token, UserResponse
 from ...neo4jConnection import get_neo4j_session
 from .services import get_password_hash, verify_password, create_access_token, create_refresh_token, get_current_user
 from jose import JWTError, jwt
+
 
 router = APIRouter(tags=["auth"])
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -14,44 +15,47 @@ ALGORITHM = os.getenv("ALGORITHM")
 @router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_user(form: RegistrationForm, session = Depends(get_neo4j_session)):
     """Register endpoint, validates form and creates a new user"""
+    # Check if username is already taken
     result = await session.run(
-        "MATCH (u:User {username: $username}) RETURN u", 
-        username=form.username
-        )
+        "MATCH (u:User) WHERE u.username = $username RETURN u",
+        username=form.username, email=form.email
+    )
     if await result.single():
-        raise HTTPException(status_code=400, detail="Username already registered")
+        raise HTTPException(status_code=400, detail="Username already taken")
     
+    # Check if email is already taken (without letting the client know)
+    # TODO: Implement email uniqueness check & send email verification
+
+    # Create user
     hashed_password = get_password_hash(form.password)
     await session.run(
-        "CREATE (u:User {username: $username, hashed_password: $password})",
-        username=form.username, password=hashed_password
-        )
+        "CREATE (u:User {username: $username, email: $email, hashed_password: $password})",
+        username=form.username, email=form.email, password=hashed_password
+    )
     return {"message": "User registration successful"}
 
 
 @router.post("/token", response_model=Token)
 async def login_for_access_token(form: OAuth2PasswordRequestForm = Depends(), session=Depends(get_neo4j_session)):
     """Login endpoint, validates form, authenticates user and creates JWT tokens"""
-    result = await session.run(
-        "MATCH (u:User {username: $username}) RETURN u", 
-        username=form.username
-        )
+    is_email = "@" in form.username
+    if is_email:
+        query = "MATCH (u:User {email: $identifier}) RETURN u"
+    else:
+        query = "MATCH (u:User {username: $identifier}) RETURN u"
+
+    result = await session.run(query, identifier=form.username)
     user = await result.single()
+
     if user is None or not verify_password(form.password, user['u']['hashed_password']):
         raise HTTPException(
             status_code=401, 
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"}
             )
-    access_token = create_access_token(
-        data={"sub": form.username}
-    )
-    refresh_token = create_refresh_token(
-        data={"sub": form.username}
-    )
     return {
-        "access_token": access_token, 
-        "refresh_token": refresh_token,
+        "access_token": create_access_token(data={"sub": form.username}), 
+        "refresh_token": create_refresh_token(data={"sub": form.username}),
         "token_type": "bearer"}
 
 
@@ -67,17 +71,16 @@ async def refresh_access_token(refresh_token: str):
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid refresh token")
 
-    access_token = create_access_token(data={"sub": username})
     return {
-        "access_token": access_token,
+        "access_token": create_access_token(data={"sub": username}),
         "refresh_token": refresh_token,
         "token_type": "bearer"
     }
 
 
-@router.get("/me")
+@router.get("/me", response_model=UserResponse)
 async def read_users_me(current_user: dict = Depends(get_current_user)):
-    return {
-        "username": current_user["username"],
-        "hashed_password": current_user["hashed_password"]
-        }
+    return UserResponse(
+        username=current_user["username"],
+        email=current_user["email"]
+    )
--- api/routes/auth/schemas.py
+++ api/routes/auth/schemas.py
@@ -1,23 +1,46 @@
 # Auth-related schemas for data validation
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr, field_validator
+import re
 
 class RegistrationForm(BaseModel):
     username: str = Field(
         ..., 
-        min_length=3, max_length=50, 
+        min_length=3, max_length=32, 
         description="The username of the user",
         example="myusername"
         )
+    email: EmailStr = Field(
+        ..., 
+        description="The email of the user",
+        example="email@example.com"
+        )
     password: str = Field(
         ..., 
-        min_length=8, 
+        min_length=8, max_length=32,
         description="The password of the user",
         example="MyP4$$w0rd"
         )
-
-class LoginForm(BaseModel):
-    username: str = Field(..., description="The username of the user")
-    password: str = Field(..., description="The password of the user")
+    
+    # validators: return 422 error + msg if validation fails
+    @field_validator("username")
+    @classmethod
+    def validate_username(cls, value):
+        if not re.match(r"^[a-zA-Z0-9_]*$", value):
+            raise ValueError("Username can only contain letters, numbers and underscores")
+        return value
+    
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Password must contain at least one lowercase letter")
+        if not re.search(r"[0-9]", value):
+            raise ValueError("Password must contain at least one digit")
+        if not re.search(r"[!?@#$%^&*]", value):
+            raise ValueError("Password must contain at least one special character")
+        return value
 
 class Token(BaseModel):
     access_token: str
@@ -26,3 +49,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str | None = None
+
+class UserResponse(BaseModel):
+    username: str
+    email: EmailStr
